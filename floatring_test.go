444617234913bb0b934f10c7d63155dd5ac5f959
ninedraft <clamores.pro@gmail.com>
2020-09-12 21:23:08 +0300

    add tests for stage 01 methods

--- floatring_test.go
+++ floatring_test.go
@@ -74,6 +74,90 @@ func testBufferConsistency(test *testing.T, n, size int) {
 	})
 }
 
+func TestBufferLen(test *testing.T) {
+	var t = func(size, expected, nWrite, nRead int) {
+		var name = fmt.Sprintf("size=%d  expected=%d  nWrite=%d  nRead=%d", size, expected, nWrite, nRead)
+		test.Run(name, func(test *testing.T) {
+			var buf = floatring.NewBuffer(size)
+			for v := 0; v < nWrite; v++ {
+				buf.WriteValue(float64(v))
+			}
+			for v := 0; v < nRead; v++ {
+				buf.ReadValue()
+			}
+			if buf.Len() != expected {
+				test.Errorf("expected len=%d, got %d", expected, buf.Len())
+			}
+		})
+	}
+
+	t(0, 0, 100, 0)
+	t(1, 1, 100, 0)
+	t(100, 100, 100, 0)
+	t(11, 11, 31, 0)
+	t(10, 5, 5, 0)
+	t(10, 6, 15, 4)
+}
+
+func TestBatchMethods(test *testing.T) {
+	var buf = floatring.NewBuffer(16)
+	var values = make([]float64, 40)
+	for i := range values {
+		values[i] = float64(i)
+	}
+	buf.Write(values)
+
+	var short = make([]float64, 4)
+	var expectedShort = []float64{24, 25, 26, 27}
+	buf.Read(short)
+	if !eq(short, expectedShort) {
+		test.Errorf("expected %#v, got %#v", expectedShort, short)
+	}
+
+	var long = make([]float64, 12)
+	var expectedLong = []float64{28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39}
+	buf.Read(long)
+	if !eq(short, expectedShort) {
+		test.Errorf("expected %#v, got %#v", expectedLong, long)
+	}
+}
+
+func TestForEach(test *testing.T) {
+	var t = func(size, input int) {
+		var name = fmt.Sprintf("size=%d  input=%d", size, input)
+		test.Run(name, func(t *testing.T) {
+			var buf = floatring.NewBuffer(size)
+			var values = testValues(input)
+			buf.Write(values)
+			var got []float64
+			buf.ForEach(func(x float64) bool {
+				got = append(got, x)
+				return true
+			})
+			if size < input {
+				values = values[input-size:]
+			}
+			if !eq(values, got) {
+				test.Errorf("expected %+v got %+v", values, got)
+			}
+		})
+	}
+
+	for size := 0; size < 100; size++ {
+		for input := 0; input < 100; input++ {
+			t(size, input)
+		}
+	}
+}
+
+func testValues(n int) []float64 {
+	var values = make([]float64, n)
+	for i := range values {
+		values[i] = float64(i)
+	}
+	return values
+}
+
 func eq(aa, bb []float64) bool {
 	if len(aa) != len(bb) {
 		return false
